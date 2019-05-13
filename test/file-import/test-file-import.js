93e9d65f69c44eb54440dfe28a82175bca7a8d63
Philipp <philippkostyra@gmail.com>
2019-05-13 13:00:05 +0200

    Skafolding of Library

--- test/file-import/test-file-import.js
+++ test/file-import/test-file-import.js
@@ -2,7 +2,16 @@ const assert = require('chai').assert;
 
 describe('Ensure that imported service description is parsable', function () {
   it('check if something fails', () => {
-    assert(false, 'should fail');
-    assert.isOk(false, 'should fail too');
+    assert.isOk(false, 'fail');
+  });
+});
+
+describe('File Importer', () => {
+  it('Ensure that given file is found', () => {
+
+  });
+
+  it('Ensure that wrong file throws exception', () => {
+    //
   });
 });
