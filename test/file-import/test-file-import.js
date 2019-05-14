3f7afa9e4eb11f50d804eff3d93046de7dbd9d68
Philipp <philippkostyra@gmail.com>
2019-05-14 13:59:09 +0200

    Importing Files done

--- test/file-import/test-file-import.js
+++ test/file-import/test-file-import.js
@@ -1,9 +1,16 @@
 const assert = require('chai').assert;
 const expect = require('chai').expect;
+const jsYaml = require('js-yaml');
 const fileImporter = require('./../../lib/file-import');
 
 describe('File Importer - Verify File', () => {
-  it('Ensure that given file is found', async () => {
+  it('Ensure that given yaml file is found', async () => {
+    let path = './test/resources/v3.0 examples/petstore.yaml';
+    let result = fileImporter.verifyFile(path);
+    expect(result).to.be.true;
+  });
+
+  it('Ensure that given file yml is found', async () => {
     let path = './test/resources/v3.0 examples/petstore.yaml';
     let result = fileImporter.verifyFile(path);
     expect(result).to.be.true;
@@ -12,5 +19,16 @@ describe('File Importer - Verify File', () => {
   it('Ensure that wrong file is not found', async () => {
     let path = './test/resources/v3.0 examples/FileNotFound.yaml';
     assert.throws(() => fileImporter.verifyFile(path));
+  });
+});
+
+describe('File Importer - Import File', () => {
+  it('Read given File as JSON', async () => {
+    let path = './test/resources/v3.0 examples/petstore.yaml';
+    let result = await fileImporter.importFile(path);
+    expect(result).to.be.an('object');
+    result = JSON.stringify(result);
+    assert.doesNotThrow(() => jsYaml.safeDump(result));
+    console.log(result);
   });
 });
